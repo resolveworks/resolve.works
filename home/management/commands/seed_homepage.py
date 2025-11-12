373163099ce62dc60ae96feedb2b9376ebd08d61
Johan Schuijt <johan@resolve.works>
2025-11-12 21:14:02 +0100

    footer

--- home/management/commands/seed_homepage.py
+++ home/management/commands/seed_homepage.py
@@ -4,7 +4,7 @@ from django.core.files.images import ImageFile
 from wagtail.models import Page, Site
 from wagtail.images.models import Image
 
-from home.models import HomePage
+from home.models import HomePage, FooterSettings
 
 
 class Command(BaseCommand):
@@ -322,6 +322,85 @@ class Command(BaseCommand):
             self.stdout.write(
                 self.style.SUCCESS(f'Updated site to use HomePage as root')
             )
+
+        # Seed footer settings
+        footer_settings, created = FooterSettings.objects.get_or_create(
+            site=site
+        )
+
+        footer_settings.heading = 'Resolve'
+
+        # Column 1: Taglines
+        footer_settings.column_1 = [
+            {
+                'type': 'paragraph',
+                'value': '<p>Makes machines empower humans</p>',
+            },
+            {
+                'type': 'paragraph',
+                'value': '<p>Made by 👨 and 🤖 in Europe</p>',
+            }
+        ]
+
+        # Column 2: Contact information
+        footer_settings.column_2 = [
+            {
+                'type': 'definition_list',
+                'value': {
+                    'items': [
+                        {
+                            'term': 'LinkedIn',
+                            'definition': '<p><a target="_blank" href="https://www.linkedin.com/in/johanschuijt/">https://www.linkedin.com/in/johanschuijt/</a></p>',
+                        },
+                        {
+                            'term': 'GitHub',
+                            'definition': '<p><a target="_blank" href="https://github.com/monneyboi/">https://github.com/monneyboi/</a></p>',
+                        },
+                        {
+                            'term': 'Email',
+                            'definition': '<p><a href="mailto:johan@resolve.works">johan@resolve.works</a></p>',
+                        },
+                        {
+                            'term': 'Phone',
+                            'definition': '<p><a href="tel:+31651952461">+31 651 952 461</a></p>',
+                        },
+                    ]
+                }
+            }
+        ]
+
+        # Column 3: Business registration
+        footer_settings.column_3 = [
+            {
+                'type': 'definition_list',
+                'value': {
+                    'items': [
+                        {
+                            'term': 'Address',
+                            'definition': '<p>Harju maakond, Tallinn, Lasnamäe linnaosa, Sepapaja tn 6, 15551</p>',
+                        },
+                        {
+                            'term': 'VAT',
+                            'definition': '<p>EE102834268</p>',
+                        },
+                        {
+                            'term': 'Register',
+                            'definition': '<p><a href="https://ariregister.rik.ee/eng/company/17154517/RESOLVE-SERVICES-OÜ" target="_blank">https://ariregister.rik.ee</a></p>',
+                        },
+                    ]
+                }
+            },
+            {
+                'type': 'paragraph',
+                'value': '<p>Estonian e-residency program</p>',
+            }
+        ]
+
+        footer_settings.save()
+
+        self.stdout.write(
+            self.style.SUCCESS('Successfully seeded footer settings!')
+        )
 
         self.stdout.write(
             self.style.SUCCESS('Successfully seeded homepage content!')
--- home/models.py
+++ home/models.py
@@ -1,8 +1,67 @@
+from django.db import models
 from wagtail.models import Page
 from wagtail.fields import StreamField
-from wagtail.admin.panels import FieldPanel
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.contrib.settings.models import BaseSiteSetting, register_setting
+from wagtail import blocks
 
-from .blocks import HeroBlock, SectionBlock
+from .blocks import HeroBlock, SectionBlock, DefinitionListBlock
+
+
+@register_setting
+class FooterSettings(BaseSiteSetting):
+    """Global footer settings."""
+
+    heading = models.CharField(
+        max_length=100,
+        default="Resolve",
+        help_text="Footer heading"
+    )
+
+    column_1 = StreamField(
+        [
+            ("paragraph", blocks.RichTextBlock(features=["bold", "italic", "link"])),
+            ("definition_list", DefinitionListBlock()),
+        ],
+        blank=True,
+        use_json_field=True,
+        help_text="First footer column content"
+    )
+
+    column_2 = StreamField(
+        [
+            ("paragraph", blocks.RichTextBlock(features=["bold", "italic", "link"])),
+            ("definition_list", DefinitionListBlock()),
+        ],
+        blank=True,
+        use_json_field=True,
+        help_text="Second footer column content"
+    )
+
+    column_3 = StreamField(
+        [
+            ("paragraph", blocks.RichTextBlock(features=["bold", "italic", "link"])),
+            ("definition_list", DefinitionListBlock()),
+        ],
+        blank=True,
+        use_json_field=True,
+        help_text="Third footer column content"
+    )
+
+    panels = [
+        FieldPanel("heading"),
+        MultiFieldPanel(
+            [
+                FieldPanel("column_1"),
+                FieldPanel("column_2"),
+                FieldPanel("column_3"),
+            ],
+            heading="Footer Columns"
+        ),
+    ]
+
+    class Meta:
+        verbose_name = "Footer"
 
 
 class HomePage(Page):
