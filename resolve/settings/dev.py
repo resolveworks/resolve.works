355f562b3ddc33b35145c87d18562024bcae892f
Johan Schuijt <johan@resolve.works>
2025-11-12 21:53:27 +0100

    django auto reload

--- resolve/settings/dev.py
+++ resolve/settings/dev.py
@@ -3,6 +3,15 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# Add django-browser-reload for development
+INSTALLED_APPS = INSTALLED_APPS + [
+    "django_browser_reload",
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
+]
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-qtfk6mvo0m_-^uz=+u=_$(&%y-dwtccjz3a6(8fsdt-c#mjj6s"
 
--- resolve/urls.py
+++ resolve/urls.py
@@ -21,6 +21,11 @@ if settings.DEBUG:
     urlpatterns += staticfiles_urlpatterns()
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+    # django-browser-reload
+    urlpatterns += [
+        path("__reload__/", include("django_browser_reload.urls")),
+    ]
+
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
     # Wagtail's page serving mechanism. This should be the last pattern in
