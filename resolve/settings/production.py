ecb3650e88c3f1b20324ee560d0dd1282d0c2254
Johan Schuijt <johan@resolve.works>
2025-12-06 00:48:45 +0100

    local nginx stack for static files

--- resolve/settings/production.py
+++ resolve/settings/production.py
@@ -1,10 +1,11 @@
 from .base import *  # noqa: F401, F403
+from .base import STORAGES
 
 # ManifestStaticFilesStorage is recommended in production, to prevent
 # outdated JavaScript / CSS assets being served from cache
 # (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/5.2/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STORAGES["staticfiles"]["BACKEND"] = (  # noqa: F405
+STORAGES["staticfiles"]["BACKEND"] = (
     "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
 )
 
